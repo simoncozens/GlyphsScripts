5f09295b7b172da75a9c4f8ff8e03e8e46b9dca4
Simon Cozens <simon@simon-cozens.org>
2015-10-30 22:50:22 +0900

    New script. glyphmonkey makes things very easy.

---
+++ Delete Close Points.py
@@ -0,0 +1,13 @@
+#MenuTitle: Delete Close Points
+# -*- coding: utf-8 -*-
+__doc__="""
+Delete points which are very close to (or on top of) other points
+"""
+from glyphmonkey import *
+
+arbitraryConstant = 5
+
+for p in Glyphs.font.selectedLayers[0].paths:
+  p.segments = [ seg for seg in p.segments if seg.length() > arbitraryConstant ]
+
+Glyphs.redraw()
