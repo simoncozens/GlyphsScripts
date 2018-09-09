0e07d5e9d8bf94429f5211b68be098e0a548b031
Simon Cozens <simon@simon-cozens.org>
2018-09-09 18:09:52 +0100

    I solved S!

--- Sansomatic.py
+++ Sansomatic.py
@@ -7,6 +7,9 @@ This is the stupidest script ever. Stupid, stupid, stupid.
 import vanilla
 from math import sin,pi,tan,atan
 
+capHeight = None
+Dimensions = None
+
 def clearLayer():
   Glyphs.font.selectedLayers[0].paths = []
 
@@ -16,6 +19,12 @@ def getControl(controls, label):
     return controls[label]["value"]
   # print("Using default for "+label, controls[label]["default"])
   return controls[label]["default"]
+
+def lerp(t,a,b):
+  return NSValue.valueWithPoint_(NSPoint(int((1-t)*a.x + t*b.x), int((1-t)*a.y + t*b.y)))
+import math
+def distance( node1, node2 ):
+    return math.hypot( node1.x - node2.x, node1.y - node2.y )
 
 def drawRectangle(x,y,w,h):
   bl = GSNode((x,y), type = LINE)
@@ -51,10 +60,6 @@ def getDimension(name):
     print("You must declare the "+name+" dimension in the palette")
     raise
   return float(Dimensions[name])
-
-masterID = Glyphs.font.selectedLayers[0].associatedMasterId
-master = Glyphs.font.masters[masterID]
-capHeight = master.capHeight
 
 options = {}
 sansomatic = {}
@@ -275,6 +280,124 @@ sansomatic["O"] = {
   },
   'method': O
 }
+
+def S(controls):
+  capHeight = master.capHeight
+
+  topwidth = getControl(controls, "Top Width")
+  bottomwidth = getControl(controls, "Bottom Width")
+
+  center = 400
+  tau = getControl(controls, "Tension")
+  alpha = getControl(controls, "Bar Straightness")
+  barHeight = getControl(controls, "Bar Height") * capHeight
+  leftThick = 55
+  rightThick = 80
+  barWidth = getControl(controls, "Bar Thickness")
+  slope = getControl(controls, "Bar Slope")
+  sign = 1
+  overshoot = getControl(controls, "Overshoot")
+  hairline = getControl(controls, "Hairline Thickness")
+
+  def doCurve(sign):
+    if sign == 1:
+      top= capHeight + overshoot
+    else:
+      top= capHeight + overshoot - hairline
+    t = GSNode((center,top), type = LINE)
+    t.smooth = True
+    
+    xleft = center - topwidth - sign * leftThick/2
+    c = GSNode((center, barHeight - sign * barWidth/2), type = LINE)
+    
+    i2 = NSPoint(xleft, top)
+    i = NSPoint(xleft, barHeight - sign * barWidth/2 + slope * (topwidth + sign * leftThick/2))
+    t1 = GSNode(lerp(tau, t.position, i2).pointValue(), type = OFFCURVE)
+    x = GSNode(lerp(alpha, c.position, i).pointValue(), type = CURVE)
+    x1 = GSNode(lerp(tau, x.position, i).pointValue(), type = OFFCURVE)
+
+    xright = center + bottomwidth - sign * rightThick/2
+    
+    i_lower = NSPoint(xright, barHeight - sign * barWidth/2 - slope * (bottomwidth-sign * rightThick/2))
+    x_lower = GSNode(lerp(alpha, c.position, i_lower).pointValue(), type = LINE)
+    x1_lower = GSNode(lerp(tau, x_lower.position, i_lower).pointValue(), type = OFFCURVE)
+    
+    
+    # yc-y=s * xc-x
+    
+    d_i2_t1 = distance(i2,t1)
+    d_x1_i = distance(x1,i)
+    
+    yl_num = d_i2_t1 * i.y  - d_x1_i * t.y + math.sqrt(d_i2_t1 * d_x1_i * i.y * i.y - 2 * d_i2_t1 * d_x1_i * i.y * t.y + d_i2_t1 * d_x1_i * t.y * t.y)
+    yl_den = d_i2_t1 - d_x1_i
+    yl = yl_num / yl_den
+    
+    
+    l = GSNode(( xleft, yl), type = CURVE)
+    l.smooth = True
+    l1 = GSNode(lerp(tau, l.position, i2).pointValue(), type = OFFCURVE)
+    l2 = GSNode(lerp(tau, l.position, i).pointValue(), type = OFFCURVE)
+    
+
+    if sign == -1:
+      base = -overshoot
+    else:
+      base = -overshoot + hairline
+
+    b = GSNode((center,base), type = CURVE)
+  #   b.smooth = True
+    
+    i2_lower = NSPoint(xright, b.y)
+    
+    b1 = GSNode(lerp(tau, b.position, i2_lower).pointValue(), type = OFFCURVE)
+    
+    l1 = GSNode(lerp(tau, l.position, i2).pointValue(), type = OFFCURVE)
+    l2 = GSNode(lerp(tau, l.position, i).pointValue(), type = OFFCURVE)
+    
+    d_i2_lower_b1 = distance(i2_lower,b1)
+    d_x1_lower_i_lower = distance(i_lower,x1_lower)
+    yr_num = d_i2_lower_b1 * i_lower.y  - d_x1_lower_i_lower * b.y - math.sqrt(d_i2_lower_b1 * d_x1_lower_i_lower * i_lower.y * i_lower.y - 2 * d_i2_lower_b1 * d_x1_lower_i_lower * i_lower.y * b.y + d_i2_lower_b1 * d_x1_lower_i_lower * b.y * b.y)
+    yr_den = d_i2_lower_b1 - d_x1_lower_i_lower
+    yr = yr_num / yr_den
+    r = GSNode(( xright, yr), type = CURVE)
+    r1 = GSNode(lerp(tau, r.position, i2_lower).pointValue(), type = OFFCURVE)
+    r2 = GSNode(lerp(tau, r.position, i_lower).pointValue(), type = OFFCURVE)
+    r.smooth = True
+    if sign == 1:
+      return [t,t1,l1,l, l2, x1,x, x_lower,x1_lower,r2,r,r1,b1,b]
+    b.type = LINE
+    x_lower.type = CURVE
+    x.type = LINE
+    t.type = CURVE
+
+    return [b, b1, r1, r, r2, x1_lower, x_lower, x, x1, l2, l, l1, t1, t]
+
+  p = GSPath()
+  n = []
+  n.extend(doCurve(1))
+  nodes2 = doCurve(-1)
+  n.extend(nodes2)
+  p.nodes = n
+  p.closed = True
+  Glyphs.font.selectedLayers[0].paths.append(p)
+  Glyphs.font.selectedLayers[0].LSB = Glyphs.font.selectedLayers[0].RSB = 50
+
+sansomatic["S"] = {
+  'controls': {
+    'Overshoot': { 'type': "Slider", 'min': 0, 'max':20, 'default': 10 },
+    'Tension': { 'type': "Slider", 'min': 0.5, 'max':0.9, 'default': 0.6 },
+    'Top Width': { 'type': "Slider", 'min': 0, 'max':700, 'default': 200 },
+    'Bottom Width': { 'type': "Slider", 'min': 0, 'max':700, 'default': 225 },
+    'Bar Straightness': { 'type': "Slider", 'min': 0, 'max':0.5, 'default': 0.2 },
+    'Bar Slope': { 'type': "Slider", 'min': 0, 'max':0.5, 'default': 0.2 },
+    'Bar Height': { 'type': "Slider", 'min': 0.5, 'max':0.7, 'default': 0.55 },
+    'Bar Thickness': { 'type': "Slider", 'min': 0, 'max':300, 'default': 120 },
+    'Hairline Thickness': { 'type': "Slider", 'min': 0, 'max':100, 'default': 20 }
+
+  },
+  'method': S
+}
+
 # T
 
 def T(controls):
@@ -374,20 +497,22 @@ sansomatic["Z"] = {
   'method': Z
 }
 
+myWindow = None
+
 def createWindow(controls, fn):
-  winWidth = 500
+  winWidth = 300
   winHeight = 40 * (1+len(controls)) + 20
-  w = vanilla.FloatingWindow((winWidth, winHeight), "Sans-o-Matic")
+  myWindow = vanilla.FloatingWindow((winWidth, winHeight), "Sans-o-Matic")
   x = 10
   y = 10
   for k in controls:
     v = controls[k]
     label = vanilla.TextBox((x,y,150,30), k)
-    x = x + 200
+    x = x + 120
     def controlCallback (s):
       for k2 in controls:
         v2 = controls[k2]
-        controls[k2]["value"] = getattr(w,k2).get()
+        controls[k2]["value"] = getattr(myWindow,k2).get()
       clearLayer()
       fn(controls)
 
@@ -402,30 +527,39 @@ def createWindow(controls, fn):
       control = vanilla.CheckBox((x,y,10,10),"",
         callback = controlCallback
       )
-    setattr(w,k,control)
-    setattr(w,k+"label",label)
+    setattr(myWindow,k,control)
+    setattr(myWindow,k+"label",label)
 
     y = y + 40
     x = 10
   def dismiss(s):
-    w.close()
-  w.dismiss   = vanilla.Button((-75, y, -15, -15), "OK", sizeStyle='regular', callback=dismiss )
+    myWindow.close()
+    myWindow = None
+  myWindow.dismiss   = vanilla.Button((-75, y, -15, -15), "OK", sizeStyle='regular', callback=dismiss )
   fn(controls)
-  w.open()
+  myWindow.open()
 
-Font = Glyphs.font
-masterID = Glyphs.font.selectedLayers[0].associatedMasterId
-glyph = Glyphs.font.selectedLayers[0].parent
-Dimensions = Font.userData["GSDimensionPlugin.Dimensions"]
-if not Dimensions:
-  print("You must set your stem widths in the dimension palette")
-Dimensions = Dimensions[masterID]
+def rebuild():
+  masterID = Glyphs.font.selectedLayers[0].associatedMasterId
+  master = Glyphs.font.masters[masterID]
+  global capHeight
+  capHeight = master.capHeight
 
-clearLayer()
+  Font = Glyphs.font
+  glyph = Glyphs.font.selectedLayers[0].parent
+  Dimensions = Font.userData["GSDimensionPlugin.Dimensions"]
+  if not Dimensions:
+    print("You must set your stem widths in the dimension palette")
+  Dimensions = Dimensions[masterID]
 
-if sansomatic.has_key(glyph.string):
-  controls = sansomatic[glyph.string]["controls"]
-  if len(controls) > 0:
-    createWindow(controls, sansomatic[glyph.string]["method"])
-else:
-  print("Don't know how to draw a "+glyph.string)
+  print(sansomatic.keys())
+  if sansomatic.has_key(glyph.string):
+    clearLayer()
+    controls = sansomatic[glyph.string]["controls"]
+    if len(controls) > 0:
+      createWindow(controls, sansomatic[glyph.string]["method"])
+  else:
+    print("Don't know how to draw a "+glyph.string)
+
+rebuild()
+NSNotificationCenter.defaultCenter.addObserver_selector_name_object(None, rebuild, "GSUpdateInterface", None)
