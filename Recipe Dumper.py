fb74f9413511869a62127cbd5cd78d83df279c0c
Simon Cozens <simon@simon-cozens.org>
2021-09-29 14:09:31 +0100

    Add title

--- Recipe Dumper.py
+++ Recipe Dumper.py
@@ -1,3 +1,9 @@
+#MenuTitle: Recipe dumper
+# -*- coding: utf-8 -*-
+__doc__="""
+Show a recipe for the selected glyphs which can be fed to "Add Glyphs..."
+"""
+
 for l in Glyphs.font.selectedLayers:
 	if l.components:
 		print("+".join(c.componentName for c in l.components)+'='+l.glyph().name)
